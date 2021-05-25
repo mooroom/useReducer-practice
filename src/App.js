a8170732e607c911706b96c3d133c3f8bbe22d3e
mooroom <61869246+mooroom@users.noreply.github.com>
2021-05-25 12:42:08 +0900

    22

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ export default function App() {
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
       <h3>Hello world!!!</h3>
+      <h2>Hi</h2>
     </div>
   );
 }
